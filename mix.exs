3eafe58f386a0ba2c23e020d1f812a272d2a33bf
Michael Truog <mjtruog@gmail.com>
2016-09-14 19:30:48 -0700

    Version 1.5.3.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Uuid.Mixfile do
 
   def project do
     [app: :uuid,
-     version: "1.5.2",
+     version: "1.5.3",
      language: :erlang,
      description: description,
      package: package,
@@ -16,7 +16,7 @@ defmodule Uuid.Mixfile do
   end
 
   defp deps do
-    [{:quickrand, "~> 1.5.2"}]
+    [{:quickrand, "~> 1.5.3"}]
   end
 
   defp description do
